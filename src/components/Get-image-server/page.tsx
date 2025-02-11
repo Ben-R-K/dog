a097dfc4c9ef53796383a90d7b2f95bf50789fbb
Ben Krüger <benx0071@edu.sde.dk>
2025-02-11 11:09:12 +0100

    Change from static class to Context provider

--- src/components/Get-image-server/page.tsx
+++ src/components/Get-image-server/page.tsx
@@ -1,7 +1,7 @@
 import { TemplateString } from "next/dist/lib/metadata/types/metadata-types";
 
 export async function GetBreedServer(breed : string | TemplateString) {
-    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+    const response = await fetch(`https://dog.ceo/api/breed/${breed.toString()}/images`)
     const data = await response.json();
     const images = Object.keys(data);
     return(
--- src/components/Render-images/page.tsx
+++ src/components/Render-images/page.tsx
@@ -1,22 +1,13 @@
-import { FC } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Button } from "@/components/ui/button";
 import { GetBreedServer } from "../Get-image-server/page";
-import { TemplateString } from "next/dist/lib/metadata/types/metadata-types";
+import { useValue } from "../SelectValue-provider/page";
 
-    interface RenderProps {
-        breed : string | TemplateString
-     }
-
-export const RenderImages : FC<RenderProps> = ({breed}) => {
-    let showImages : boolean = false;
+export const RenderImages = () => {
+    const Value = useValue();
     return(
-        <div>
-        <ScrollArea>
-            if({showImages}) 
-            {GetBreedServer(`${breed}`)}
-        </ScrollArea>
-        </div>        
+        <ScrollArea> 
+            {GetBreedServer(`${Value.Elements.Value}`)}
+        </ScrollArea>       
     );
 }
 RenderImages.displayName = "RenderImages";
