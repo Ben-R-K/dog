49a0f359c1a01baeb2dc742f017ee2bd1ca63128
Ben Krüger <benx0071@edu.sde.dk>
2025-02-12 17:11:03 +0100

    Fixed design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,21 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="min-h-screen m-44">
-      <main className="grid grid-rows-3 grid-flow-col gap-6 justify-items-center">
+      <main className="grid grid-rows-1 grid-cols-5 grid-flow-col justify-items-stretch">
+        <div className="grid grid-rows-3 grid-flow-col gap-6 col-start-3 justify-self-center max-h-48">
         <Input className="max-w-60" placeholder="Antal billeder"/>
        <Select>
-        <SelectTrigger className="w-60">
+        <SelectTrigger className="max-w-60">
           <SelectValue className="text-slate-900" placeholder="Velg en hunderace"/>
         </SelectTrigger>
         <SelectContent>
          <BreedsServer/>
         </SelectContent>
         </Select>
-        <Button className="font-semibold min-w-60">Søg efter hunderacen</Button>
-        <ScrollArea>
-          {GetBreedServer("affenpinscher")}
+        <Button className="font-semibold max-w-60">Søg efter hunderacen</Button>
+        </div>
+        <ScrollArea className="max-h-screen row-span-3 col-span-2 col-start-4 justify-self-end overflow-auto">
+            {GetBreedServer("african")}
         </ScrollArea>
       </main>
     </div>
